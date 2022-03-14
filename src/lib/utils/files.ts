59f9a9314bf73515b0661268625f91dcfc115dde
Dako_ <imdakooo@gmail.com>
2022-03-14 06:14:30 +0100

    +

--- src/lib/utils/files.ts
+++ src/lib/utils/files.ts
@@ -7,6 +7,6 @@ import { readFileSync } from 'node:fs';
  * @param {string} path Path to a `.yaml` file.
  * @returns {Object} Parsed JSON.
  */
- export function parseYamlFile(path: string): Object {
+export function parseYamlFile(path: string): Object {
 	return parse(readFileSync(normalize(path), { encoding: 'utf-8' }));
 }
